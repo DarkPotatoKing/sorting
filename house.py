83b79fe7e8d6038575512ea9c362ad646b619004
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-19 17:50:29 +0800

    added config file to gitignore

--- house.py
+++ house.py
@@ -1,28 +1,32 @@
 class House:
-    # -1 slots means no limit on slots
-    slots_with_laptop = 0
-    slots_without_laptop = -1
-    max_slots = 0
-    slots_taken = 0
-    students = []
-    name = ''
 
     def __init__(self, name = '', max_slots = 0, slots_without_laptop = -1, slots_with_laptop = 0):
         self.max_slots = max_slots
         self.slots_without_laptop = slots_without_laptop
         self.slots_with_laptop = slots_with_laptop
         self.name = str(name)
+        self.slots_taken = 0
+        self.students = []
 
 
     def add_student_with_laptop(self, name):
-        slots_taken += 1
-        slots_with_laptop -= 1
-        students.append(str(name))
+        self.slots_taken += 1
+        self.slots_with_laptop -= 1
+        self.students.append(str(name))
+        print '{} added to {}.'.format(name, self.name)
+        # print self.students
 
     def add_student_without_laptop(self, name):
-        slots_taken += 1
-        slots_without_laptop -= 1
-        students.append(str(name))
+        self.slots_taken += 1
+        self.slots_without_laptop -= 1
+        self.students.append(str(name))
+        print '{} added to {}.'.format(name, self.name)
+        # print self.students
+
+    def print_students(self):
+        print self.name
+        for x in self.students:
+            print x
 
     def __repr__(self):
         return '{}\tLaptops: {}\tPCs: {}\tMax Slots: {}'.format(self.name, str(self.slots_with_laptop),str(self.slots_without_laptop),str(self.max_slots))
