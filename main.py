82e77ec0dec76a05defb73210f78c3e96528a4b1
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-19 22:15:41 +0800

    added command line arguments handling

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from sorting_hat import SortingHat
+import sys
 
 def main():
     s = SortingHat()
-    s.start()
+    code = 0
+
+    if len(sys.argv) > 1:
+        code = int(sys.argv[1])
+
+    if code == 0:
+        s.start()
+    elif code == 1:
+        s.load()
+        s.sort_students()
+    else:
+        print 'Invalid code!'
 
 
 if __name__ == '__main__':
