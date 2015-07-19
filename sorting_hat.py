cfac9b68ba245efd0e5595d26cf19230a3145d30
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-19 15:51:33 +0800

    added assign house slots method

--- sorting_hat.py
+++ sorting_hat.py
@@ -1,13 +1,18 @@
+from __future__ import division
+from house import House
+import math
+
 class SortingHat:
     num_students = 0
-    slytherin = None
-    gryffindor  = None
-    ravenclaw = None
-    hufflepuff = None
+    slytherin = House()
+    gryffindor  = House()
+    ravenclaw = House()
+    hufflepuff = House()
+    
 
     def __init__(self):
         self.load_config()
-        print self.num_students
+        self.assign_house_slots()
 
     def load_config(self):
         lines = []
@@ -22,3 +27,21 @@ class SortingHat:
             config[key] = val
 
         self.num_students = int(config['num_students'])
+
+    def assign_house_slots(self):
+        max_students_per_house = int(math.ceil(self.num_students / 4))
+
+        # purely laptop class
+        self.slytherin = House(max_students_per_house, 0, -1)
+
+        # 10 slots without laptop, the rest with laptops
+        self.ravenclaw = House(max_students_per_house, 10, -1)
+
+        # normal labs
+        self.hufflepuff = House(max_students_per_house)
+        self.gryffindor = House(max_students_per_house)
+
+        self.houses = [self.slytherin, self.ravenclaw, self.hufflepuff, self.gryffindor]
+        
+        for x in self.houses:
+            print x
