ac13a54882f3f78bc3c42ba8cb33fdc610fb2f7b
Kyle Rosales <kyle.rosales97@gmail.com>
2015-07-19 16:23:09 +0800

    added name attribute to house

--- house.py
+++ house.py
@@ -5,11 +5,13 @@ class House:
     max_slots = 0
     slots_taken = 0
     students = []
+    name = ''
 
-    def __init__(self, max_slots = 0, slots_without_laptop = -1, slots_with_laptop = 0):
+    def __init__(self, name = '', max_slots = 0, slots_without_laptop = -1, slots_with_laptop = 0):
         self.max_slots = max_slots
         self.slots_without_laptop = slots_without_laptop
         self.slots_with_laptop = slots_with_laptop
+        self.name = str(name)
 
 
     def add_student_with_laptop(self, name):
@@ -23,4 +25,4 @@ class House:
         students.append(str(name))
 
     def __repr__(self):
-        return 'Laptops: {}\tPCs: {}\tMax Slots: {}'.format(str(self.slots_with_laptop),str(self.slots_without_laptop),str(self.max_slots))
+        return '{}\tLaptops: {}\tPCs: {}\tMax Slots: {}'.format(self.name, str(self.slots_with_laptop),str(self.slots_without_laptop),str(self.max_slots))
